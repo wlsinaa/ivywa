427008a780612e94582460b745ca49a0ea78969a
wlsinaa <45190489+wlsinaa@users.noreply.github.com>
2022-12-21 12:40:58 -0500

    unused file

--- dfs.py
+++
@@ -1,5 +0,0 @@
-def dfs(self, node, parent, grandparent):
-    if grandparent and grandparent.val % 2 == 0:
-        self.sumG += node.val
-    if node.left: self.dfs(node.left, node, parent)
-    if node.right: self.dfs(node.right, node, parent)
